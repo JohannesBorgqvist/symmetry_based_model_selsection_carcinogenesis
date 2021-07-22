e4b0e51659dac8954aa0472041a670bddb86d7d0
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2021-07-22 08:35:04 +0100

    Add supplementary

---
+++ Supplementary/Input/auto/derivation_symmetries.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook
+ "derivation_symmetries"
+ (lambda ()
+   (LaTeX-add-labels
+    "eq:exp"
+    "eq:power_law"
+    "eq:mixed"))
+ :latex)
+
--- Supplementary/auto/supplementary_material_symmetry_carcinogenesis.el
+++ Supplementary/auto/supplementary_material_symmetry_carcinogenesis.el
@@ -1,18 +1,39 @@
 (TeX-add-style-hook
  "supplementary_material_symmetry_carcinogenesis"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "margin=0.7in") ("parskip" "parfill") ("inputenc" "utf8")))
+                     '(("geometry" "margin=1in") ("parskip" "parfill") ("inputenc" "utf8") ("fontenc" "T1") ("helvet" "scaled=1") ("sfmath" "helvet") ("footmisc" "symbol")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
+    "./Input/derivation_symmetries"
     "article"
-    "art10"
+    "art12"
     "geometry"
     "parskip"
     "inputenc"
+    "fontenc"
+    "helvet"
     "amsmath"
     "amssymb"
     "amsfonts"
-    "amsthm"))
+    "amsthm"
+    "mathtools"
+    "sfmath"
+    "physics"
+    "footmisc"
+    "url"
+    "graphicx")
+   (LaTeX-add-labels
+    "Oxford"
+    "WennerGren"
+    "Linacre")
+   (LaTeX-add-bibliographies
+    "model_selection_symmetries_cancer"))
  :latex)
 
