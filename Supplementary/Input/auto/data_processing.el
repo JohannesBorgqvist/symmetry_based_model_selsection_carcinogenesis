44667fae0dbd655bec5c490b13a4bb7e9c54ff21
Johannes Borgqvist <borgqvist@maths.ox.ac.uk>
2021-12-15 15:15:18 +0000

    Remove Figures, Draft and Supplemenary folders

--- Supplementary/Input/auto/data_processing.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "data_processing"
- (lambda ()
-   (LaTeX-add-labels
-    "fig:data_processing"))
- :latex)
-
--- Supplementary/Input/auto/num_val.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "num_val"
- (lambda ()
-   (LaTeX-add-labels
-    "eq:scaling"
-    "fig:numerical_validation"))
- :latex)
-
