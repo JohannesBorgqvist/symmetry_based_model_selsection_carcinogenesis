939cdb1a134d9ac6d4aa3f6cfb9c106ac52a5fa7
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2022-02-12 17:01:06 +0000

    Illustrate symmetries

--- Code/fit_to_data.py
+++ Code/fit_to_data.py
@@ -31,28 +31,27 @@ def objective_PLM(parameters,t):
     # Return the output
     return A*(t**gamma)
 # ---------------------------------------------------------------------------------------
-# Function 2: "objective_IM_II"
+# Function 2: "objective_IM_III"
 # The function returns the objective value of the exponential model and it takes
 # the following three inputs
 # 1."t" being the ages in the time series (or the independent variable if you will),
 # 2. "A" being the scaling parameter in all models,
 # 3. "tau" being the delay parameter.
-def objective_IM_II(parameters, t):
+def objective_IM_III(parameters, t):
     # Define the constant alpha
-    #alpha = 0.044
+    alpha = 0.044
     # Extract the parameters to be fitted
-    A, tau, C, alpha = parameters
+    A, tau, C = parameters
     #A, tau, C = parameters
     # Return the logarithm of the output
-    #return ((A)/(np.exp(np.exp(-alpha*(t-tau)))-1))
-    return ((A)/(np.exp(np.exp(-alpha*(t-tau)))-C*A))
+    return ((A)/(np.exp(np.exp(-alpha*(t-tau)))-C))
 # ---------------------------------------------------------------------------------------
 # Function 3: "PE_risk_profiles"
-# The function fits one of the two candidate models (the PLM or the IM-II) to experimental
+# The function fits one of the two candidate models (the PLM or the IM-III) to experimental
 # data. It takes four inputs:
 # 1. The vector t being the ages of the patients,
 # 2. The vector R being the number of incidences of cancer for a given age,
-# 3. The string model_str defining whether it is the PLM or the IM-II that is fitted to the data,
+# 3. The string model_str defining whether it is the PLM or the IM-III that is fitted to the data,
 # 4. The string fit_type defining whether it is the standard least square or the Orthogonal Distance Regression
 # (ODR) measure that is used.
 # The function returns three outputs:
@@ -62,17 +61,14 @@ def objective_IM_II(parameters, t):
 def PE_risk_profiles(t, R, model_str,fit_string,fixed_parameters):
     # Define the data at hand as a structure on which
     # the scipy odr package will do the estimation
-    #data = RealData(t, R, 0.03, 0.01)
-    #data = RealData(t, R)
-    #data = Data(t, R, 0.01, 0.01)
     data = Data(t, R)
     # Define the number of start guesses we will try. Note that the optimisation for finding
     # the optimal parameters is local, and the cost function is non-convex meaning that there
     # are multiple local optima which can be found for different start guesses. Therefore, 
     # we use a multiple shooting technique were we test multiple start guesses and then we
     # save the optimal parameters that resulted in the best fit.
-    num_of_start_guesses = 7
-    # We have two models to consider, namely the PLM and the IM-II. In order to do the ODR
+    num_of_start_guesses = 10
+    # We have two models to consider, namely the PLM and the IM-III. In order to do the ODR
     # based model fitting, we need to construct a model object and a start guess for the
     # parameters.
     if model_str == "PLM":# The PLM
@@ -82,17 +78,14 @@ def PE_risk_profiles(t, R, model_str,fit_string,fixed_parameters):
         # where we do many local optimisations starting from these startguesses.
         # In the end, we pick the parameters resulting in the lowest minima. This
         # is because we have a non-convex optimisation problem with many local minima.
-        A_vec = np.linspace(0.001,10,num_of_start_guesses,endpoint=True)
+        A_vec = np.linspace(0.00001,1,num_of_start_guesses,endpoint=True)
         gamma_vec = np.linspace(1,10,num_of_start_guesses,endpoint=True)        
         # Extract the first start guess
         parameter_guesses = [[A, gamma] for A in A_vec for gamma in gamma_vec]
-        #parameter_guess = [1 , 5]
-        #parameter_guess = [0.02, 3]
-    elif model_str == "IM-II": # The IM-II
+    elif model_str == "IM-III": # The IM-III
         # Define the model
-        model = Model(objective_IM_II)
-        # Define the start guess [A, tau, C, alpha]
-        #parameter_guess = [1.5, 20, 0.1, 0.044] # Excellent start guess
+        model = Model(objective_IM_III)
+        # Define the start guess [A, tau, C, ]
         # Define a set of parameter values for the multishooting technique
         # where we do many local optimisations starting from these startguesses.
         # In the end, we pick the parameters resulting in the lowest minima. This
@@ -100,16 +93,15 @@ def PE_risk_profiles(t, R, model_str,fit_string,fixed_parameters):
         A_vec = np.linspace(0.01,50,num_of_start_guesses,endpoint=True)
         C_vec = np.linspace(0.01,10,num_of_start_guesses,endpoint=True)
         tau_vec = np.linspace(1,200,num_of_start_guesses,endpoint=True)
-        alpha_vec = np.linspace(0.001,0.1,num_of_start_guesses,endpoint=True)                
         # Extract the first start guess
-        parameter_guesses = [[A, tau, C, alpha] for A in A_vec for tau in tau_vec for C in C_vec for alpha in alpha_vec]        
+        parameter_guesses = [[A, tau, C] for A in A_vec for tau in tau_vec for C in C_vec]        
     # Set an initial value of the RMS so that it will be updated    
     RMS = 50000
     # Also, initiate the other two outputs that we return
     fitted_model = 0
     R_hat = np.array([1, 2, 3])
     #------------------------------------------------------------------------------------------
-    # Now we re-do this analysis for the remaining start guesses of the parameters
+    # Now we do this analysis for the remaining start guesses of the parameters
     for parameter_guess in parameter_guesses:
         # Define the model fitting object
         if len(fixed_parameters)==0:
@@ -128,8 +120,8 @@ def PE_risk_profiles(t, R, model_str,fit_string,fixed_parameters):
         # Generate the fitted time series as well in the two cases
         if model_str == "PLM":# The PLM    
             R_hat_temp = np.array([objective_PLM(fitted_model_temp.beta,t_i) for t_i in list(t)])
-        elif model_str == "IM-II": # The IM-II
-            R_hat_temp = np.array([objective_IM_II(fitted_model_temp.beta,t_i) for t_i in list(t)])
+        elif model_str == "IM-III": # The IM-III
+            R_hat_temp = np.array([objective_IM_III(fitted_model_temp.beta,t_i) for t_i in list(t)])
         # Calculate the RMS
         RMS_temp = np.sqrt(((fitted_model_temp.sum_square)/(len(R))))
         # Lastly, if we obtained a better fit than the current minimal fit, we save that fit instead.
--- Code/symmetry_toolbox.py
+++ Code/symmetry_toolbox.py
@@ -1,7 +1,7 @@
 # =================================================================================
 # =================================================================================
 # Script:"symmetry_based_model_selection"
-# Date: 2021-09-20
+# Date: 2022-02-11
 # Implemented by: Johannes Borgqvist
 # Description:
 # The script involves all the symmetry functionalities
@@ -12,7 +12,7 @@
 # =================================================================================
 import read_data  # Home-made
 import write_output  # Home-made
-import fit_to_data_ODR # Home-made
+import fit_to_data # Home-made
 import numpy as np 
 from matplotlib import pyplot as plt
 # =================================================================================
@@ -20,23 +20,19 @@ from matplotlib import pyplot as plt
 # Functions
 # =================================================================================
 # =================================================================================
-# FUNCTION 1: PLM_1_symmetry
-# The first symmetry of the power law model (PLM)
-def PLM_1_symmetry(t,R,epsilon,gamma):
-    t_hat = t
-    R_hat = R + epsilon*(t**gamma)
-    return t_hat,R_hat
-# FUNCTION 2: PLM_2_symmetry
+#----------------------------------------------------------------------------------
+# FUNCTION 1: PLM_symmetry
 # The second symmetry of the power law model (PLM)
-def PLM_2_symmetry(t,R,epsilon):
+def PLM_symmetry(t,R,epsilon):
     t_hat = t * np.exp(epsilon)
     R_hat = R
     return t_hat,R_hat
-# FUNCTION 3: PLM_transformation
+#----------------------------------------------------------------------------------
+# FUNCTION 2: PLM_transformation
 # The function plots the action of
 # the symmetries of the PLM from
 # a given starting point and an end point
-def PLM_transformation(t_from,R_from,epsilon,gamma,indicator):
+def PLM_transformation(t_from,R_from,epsilon,gamma):
     # In order to plot the action of the transformation
     # continuously we will add small plots along the
     # way.
@@ -49,12 +45,7 @@ def PLM_transformation(t_from,R_from,epsilon,gamma,indicator):
     t_trans_list.append(t_from)
     R_trans_list.append(R_from)
     # Calculate the transformed point of interest
-    if indicator == 1:
-        # Calculate the next point for the transformation
-        t_to, R_to = PLM_1_symmetry(t_from,R_from,epsilon_increment,gamma)
-    elif indicator == 2:
-        # Calculate the next point for the transformation
-        t_to, R_to = PLM_2_symmetry(t_from,R_from,epsilon_increment)        
+    t_to, R_to = PLM_symmetry(t_from,R_from,epsilon_increment)        
     # Save the new rotated point
     t_trans_list.append(t_to)
     R_trans_list.append(R_to)
@@ -65,12 +56,7 @@ def PLM_transformation(t_from,R_from,epsilon,gamma,indicator):
         t_from = t_to
         R_from = R_to
         # Calculate the transformed point of interest
-        if indicator == 1:
-            # Next point for the transformation
-            t_to, R_to = PLM_1_symmetry(t_from,R_from,epsilon_increment,gamma)
-        elif indicator == 2:
-            # Calculate the next point for the transformation
-            t_to, R_to = PLM_2_symmetry(t_from,R_from,epsilon_increment)                    
+        t_to, R_to = PLM_symmetry(t_from,R_from,epsilon_increment)                    
         # Save the rotated point
         t_trans_list.append(t_to)
         R_trans_list.append(R_to)
@@ -81,20 +67,33 @@ def PLM_transformation(t_from,R_from,epsilon,gamma,indicator):
     R_trans = np.asarray(R_trans_list)
     # Return the action of the transformation
     return t_trans,R_trans
-# FUNCTION 4: IM_II_symmetry
-# The symmetry of the immunological model IM-II
-def IM_II_symmetry(t,R,epsilon,tau,alpha):
+#----------------------------------------------------------------------------------
+# FUNCTION 3: PLM_transformed_solution
+# This function returns the transformed solution of the PLM for a given value
+# of the parameters A and gamma as well as the transformation parameter epsilon
+def PLM_transformed_solution(t,R,epsilon,A,gamma):
+    # Allocate memory for the transformed solution
+    #t_trans = t 
+    t_trans = np.asarray([PLM_symmetry(t[index],R[index],epsilon)[0] for index in range(len(t))])
+    #R_trans = np.asarray([fit_to_data.objective_PLM((A*np.exp(-gamma*epsilon),gamma),t_i) for t_i in list(t_trans)])
+    R_trans = R
+    # Return the transformed solution
+    return t_trans,R_trans
+#----------------------------------------------------------------------------------
+# FUNCTION 4: IM_III_symmetry
+# The symmetry of the immunological model IM-III
+def IM_III_symmetry(t,R,epsilon,tau,alpha):
     # Define t_hat recursively
-    #t_hat =  np.log(np.log(np.abs(np.exp(np.exp(-alpha*(t-tau)))-(alpha*np.exp(alpha*tau)*epsilon))))
     t_hat =  np.log(np.log(np.exp(np.exp(-alpha*(t-tau))) - (alpha*np.exp(alpha*tau)*epsilon) ))
     t_hat = tau - ((t_hat)/(alpha))
     R_hat = R
     return t_hat,R_hat
-# FUNCTION 5: PLM_transformation
+#----------------------------------------------------------------------------------
+# FUNCTION 5: IM_III_transformation
 # The function plots the action of
-# the symmetries of the PLM from
+# the symmetries of the IM-III from
 # a given starting point and an end point
-def IM_II_transformation(t_from,R_from,epsilon,tau,alpha):
+def IM_III_transformation(t_from,R_from,epsilon,tau,alpha):
     # In order to plot the action of the transformation
     # continuously we will add small plots along the
     # way.
@@ -107,7 +106,7 @@ def IM_II_transformation(t_from,R_from,epsilon,tau,alpha):
     t_trans_list.append(t_from)
     R_trans_list.append(R_from)
     # Calculate the transformed point of interest
-    t_to, R_to = IM_II_symmetry(t_from,R_from,epsilon_increment,tau,alpha)
+    t_to, R_to = IM_III_symmetry(t_from,R_from,epsilon_increment,tau,alpha)
     # Save the new rotated point
     t_trans_list.append(t_to)
     R_trans_list.append(R_to)
@@ -118,7 +117,7 @@ def IM_II_transformation(t_from,R_from,epsilon,tau,alpha):
         t_from = t_to
         R_from = R_to
         # Calculate the next transformed point
-        t_to, R_to = IM_II_symmetry(t_from,R_from,epsilon_increment,tau,alpha)
+        t_to, R_to = IM_III_symmetry(t_from,R_from,epsilon_increment,tau,alpha)
         # Save the rotated point
         t_trans_list.append(t_to)
         R_trans_list.append(R_to)
@@ -129,67 +128,21 @@ def IM_II_transformation(t_from,R_from,epsilon,tau,alpha):
     R_trans = np.asarray(R_trans_list)
     # Return the action of the transformation
     return t_trans,R_trans
-# FUNCTION 6: IM_I_1_symmetry
-# The first symmetry of the exponential model (IM-I)
-def IM_I_1_symmetry(t,R,epsilon,alpha):
-    t_hat = t
-    R_hat = R + epsilon*(np.exp(alpha*t))
-    return t_hat,R_hat
-# FUNCTION 7: IM_I_2_symmetry
-# The second symmetry of the exponential model (IM-I)
-def IM_I_2_symmetry(t,R,epsilon):
-    t_hat = t + epsilon
-    R_hat = R
-    return t_hat,R_hat
-# FUNCTION 8: PLM_transformation
-# The function plots the action of
-# the symmetries of the PLM from
-# a given starting point and an end point
-def IM_I_transformation(t_from,R_from,epsilon,alpha,indicator):
-    # In order to plot the action of the transformation
-    # continuously we will add small plots along the
-    # way.
-    epsilon_increment = epsilon/10 # Increment rotation angle
-    epsilon_temp = epsilon_increment # The current angle we plot with
-    # Two lists as output corresponding to the transformation
-    t_trans_list = [] # Transformed variable
-    R_trans_list = [] # Transformed state
-    # Append the starting point
-    t_trans_list.append(t_from)
-    R_trans_list.append(R_from)
-    # Calculate the transformed point of interest
-    if indicator == 1:
-        # Calculate the next point for the transformation
-        t_to, R_to = IM_I_1_symmetry(t_from,R_from,epsilon_increment,alpha)
-    elif indicator == 2:
-        # Calculate the next point for the transformation
-        t_to, R_to = IM_I_2_symmetry(t_from,R_from,epsilon_increment)        
-    # Save the new rotated point
-    t_trans_list.append(t_to)
-    R_trans_list.append(R_to)
-    # Re-do this procedure until we have rotated the original point
-    # far enough
-    while abs(epsilon_temp)<abs(epsilon):
-        # Update point we want to rotate
-        t_from = t_to
-        R_from = R_to
-        # Calculate the transformed point of interest
-        if indicator == 1:
-            # Next point for the transformation
-            t_to, R_to = IM_I_1_symmetry(t_from,R_from,epsilon_increment,alpha)
-        elif indicator == 2:
-            # Calculate the next point for the transformation
-            t_to, R_to = IM_I_2_symmetry(t_from,R_from,epsilon_increment)                    
-        # Save the rotated point
-        t_trans_list.append(t_to)
-        R_trans_list.append(R_to)
-        # Change the angle we rotate with
-        epsilon_temp += epsilon_increment
-    # Convert the lists to arrays
-    t_trans = np.asarray(t_trans_list)
-    R_trans = np.asarray(R_trans_list)
-    # Return the action of the transformation
+#----------------------------------------------------------------------------------
+# FUNCTION 8: IM_III_transformed_solution
+# This function returns the transformed solution of the IM-III for a given value
+# of the parameters A, tau and C as well as the transformation parameter epsilon
+def IM_III_transformed_solution(t,R,epsilon,A,tau,C):
+    # Define alpha to its fixed value
+    alpha = 0.044
+    # Allocate memory for the transformed solution
+    #t_trans = t
+    t_trans = np.asarray([IM_III_symmetry(t[index],R[index],epsilon,tau,alpha)[0] for index in range(len(t))])
+    #R_trans = np.asarray([fit_to_data.objective_IM_III((A,tau,C-alpha*np.exp(alpha*tau)*(epsilon)),t_i) for t_i in list(t_trans)])
+    R_trans = R
+    # Return the transformed solution
     return t_trans,R_trans
+#----------------------------------------------------------------------------------
 # FUNCTION 9: sym_model_sel
 # The function conducts the symmetry based model selection.
 # It takes the following input:
@@ -206,7 +159,7 @@ def sym_model_sel(t,R,epsilon_vector,opt_para,model_str):
     # Allocate memory for the output
     Delta_fit = []
     # Calculate the original fit
-    R_hat, opt_para_original, R_squared_adj_original = fit_to_data_ODR.PE_risk_profiles(t,R,model_str,opt_para)
+    R_hat, opt_para_original, R_squared_adj_original = fit_to_data.PE_risk_profiles(t,R,model_str,opt_para)
     # Re-define data by taking the exponential of it
     R_ori_data = [np.exp(R[i]) for i in range(len(R))]
     # Extract the value of alpha
@@ -244,7 +197,7 @@ def sym_model_sel(t,R,epsilon_vector,opt_para,model_str):
         t_trans_data = np.array(t_trans_data)
         R_trans_data_log = np.array(R_trans_data_log)
         # Calculate the fit
-        R_hat_trans, opt_para_temp, R_squared_adj_after = fit_to_data_ODR.PE_risk_profiles(t_trans_data,R_trans_data_log,model_str,opt_para)
+        R_hat_trans, opt_para_temp, R_squared_adj_after = fit_to_data.PE_risk_profiles(t_trans_data,R_trans_data_log,model_str,opt_para)
         # Append the relative fit
         #Delta_fit.append(((R_squared_adj_after)/(R_squared_adj_original)))
         Delta_fit.append(R_squared_adj_after)
