fbbd8312185e64fed4f3ebfdfab6344040d3a323
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2022-03-14 20:44:59 +0000

    Add higher density in RMS vs epsilon. Now, we work with 5*epsilon_scale with 3000 points, and before it was 2000 points.

--- Code/symmetry_analysis_PLM_vs_IM_III.py
+++ Code/symmetry_analysis_PLM_vs_IM_III.py
@@ -204,7 +204,7 @@ print("\t\t\tIM-III\t CML:\t epsilon_scale\t=\t%0.12f\n\n"%(5*epsilon_scale_IM_I
 # Allocate four epsilon vectors with transformation parameters
 #epsilon_vector_PLM = np.array([0.0,30*epsilon_scale_PLM])
 #epsilon_vector_PLM = np.array([0.0,epsilon_scale_PLM])
-epsilon_vector_PLM = np.linspace(0,5*epsilon_scale_PLM,2000,endpoint=True)
+epsilon_vector_PLM = np.linspace(0,5*epsilon_scale_PLM,3000,endpoint=True)
 #--------------------------------------------------------------------------------------------------------
 #epsilon_vector_PLM = np.array([0.0,0.2*epsilon_scale_PLM])
 #epsilon_vector_IM_III_myeloma = np.array([0,0.6795*epsilon_scale_IM_III_myeloma])
@@ -217,9 +217,9 @@ epsilon_vector_PLM = np.linspace(0,5*epsilon_scale_PLM,2000,endpoint=True)
 #epsilon_vector_IM_III_colon = np.array([0,0.5*epsilon_scale_IM_III_colon,0.55*epsilon_scale_IM_III_colon,0.60*epsilon_scale_IM_III_colon,0.65*epsilon_scale_IM_III_colon,0.70*epsilon_scale_IM_III_colon,0.75*epsilon_scale_IM_III_colon,0.80*epsilon_scale_IM_III_colon])
 #epsilon_vector_IM_III_CML = np.array([0,0.745*epsilon_scale_IM_III_CML,0.75*epsilon_scale_IM_III_CML,0.80*epsilon_scale_IM_III_CML,0.85*epsilon_scale_IM_III_CML,0.90*epsilon_scale_IM_III_CML,0.95*epsilon_scale_IM_III_CML,epsilon_scale_IM_III_CML])
 #--------------------------------------------------------------------------------------------------------
-epsilon_vector_IM_III_myeloma = np.linspace(0,5*epsilon_scale_IM_III_myeloma,2000,endpoint=True)
-epsilon_vector_IM_III_colon = np.linspace(0,5*epsilon_scale_IM_III_colon,2000,endpoint=True)
-epsilon_vector_IM_III_CML = np.linspace(0,5*epsilon_scale_IM_III_CML,2000,endpoint=True)
+epsilon_vector_IM_III_myeloma = np.linspace(0,5*epsilon_scale_IM_III_myeloma,3000,endpoint=True)
+epsilon_vector_IM_III_colon = np.linspace(0,5*epsilon_scale_IM_III_colon,3000,endpoint=True)
+epsilon_vector_IM_III_CML = np.linspace(0,5*epsilon_scale_IM_III_CML,3000,endpoint=True)
 #--------------------------------------------------------------------------------------------------------
 # Create an iterable list
 input_lists = [[t_myeloma,R_myeloma,epsilon_vector_PLM,PLM_fitted_to_myeloma_ODR,"PLM"],[t_myeloma,R_myeloma,epsilon_vector_IM_III_myeloma,IM_III_fitted_to_myeloma_ODR,"IM-III"],[t_colon,R_colon,epsilon_vector_PLM,PLM_fitted_to_colon_ODR,"PLM"],[t_colon,R_colon,epsilon_vector_IM_III_colon,IM_III_fitted_to_colon_ODR,"IM-III"],[t_CML,R_CML,epsilon_vector_PLM,PLM_fitted_to_CML_ODR,"PLM"],[t_CML,R_CML,epsilon_vector_IM_III_CML,IM_III_fitted_to_CML_ODR,"IM-III"]]
@@ -444,21 +444,25 @@ write_output.plot_LaTeX_2D(epsilon_transf_IM_III_CML_sparse,RMS_transf_IM_III_CM
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 print("\t\tIllustrate framework!\n")
-print("\t\tPLM\n")
-print("\t\t\tPLM original parameters:\t%s"%(str(fitted_parameters_PLM_colon[0])))
-print("\t\t\tPLM inverse parameters:\t%s"%(str(inverse_parameters_PLM_colon[len(inverse_parameters_PLM_colon)-1])))
-print("\t\t\tPLM original fit:\t%s"%(str(RMS_transf_PLM_colon[0])))
-print("\t\t\tPLM inverse fit:\t%s"%(str(RMS_transf_PLM_colon[len(RMS_transf_PLM_colon)-1])))
-print("\t\tIM-III\n")
-print("\t\t\tIM-III original parameters:\t%s"%(str(fitted_parameters_IM_III_colon[0])))
-print("\t\t\tIM-III inverse parameters:\t%s"%(str(inverse_parameters_IM_III_colon[len(inverse_parameters_IM_III_colon)-1])))
-print("\t\t\tIM-III original fit:\t%s"%(str(RMS_transf_IM_III_colon[0])))
-print("\t\t\tIM-III inverse fit:\t%s"%(str(RMS_transf_IM_III_colon[len(RMS_transf_IM_III_colon)-1])))
+#print("\t\tPLM\n")
+#print("\t\t\tPLM original parameters:\t%s"%(str(fitted_parameters_PLM_colon[0])))
+#print("\t\t\tPLM inverse parameters:\t%s"%(str(inverse_parameters_PLM_colon[len(inverse_parameters_PLM_colon)-1])))
+#print("\t\t\tPLM original fit:\t%s"%(str(RMS_transf_PLM_colon[0])))
+#print("\t\t\tPLM inverse fit:\t%s"%(str(RMS_transf_PLM_colon[len(RMS_transf_PLM_colon)-1])))
+#print("\t\tIM-III\n")
+#print("\t\t\tIM-III original parameters:\t%s"%(str(fitted_parameters_IM_III_colon[0])))
+#print("\t\t\tIM-III inverse parameters:\t%s"%(str(inverse_parameters_IM_III_colon[len(inverse_parameters_IM_III_colon)-1])))
+#print("\t\t\tIM-III original fit:\t%s"%(str(RMS_transf_IM_III_colon[0])))
+#print("\t\t\tIM-III inverse fit:\t%s"%(str(RMS_transf_IM_III_colon[len(RMS_transf_IM_III_colon)-1])))
 #------------------------------------------------------------------------------
 # STEP 1 OUT OF 4: TRANSFORM DATA
 #------------------------------------------------------------------------------
+# Find the index closest to the epsilon scale of the PLM
+index_PLM_colon = np.where(epsilon_transf_PLM_colon==epsilon_transf_PLM_colon[np.abs(epsilon_transf_PLM_colon-epsilon_scale_PLM).argmin()])[0][0]
 # Extract out epsilon value
-epsilon = epsilon_transf_PLM_colon[len(epsilon_transf_PLM_colon)-1]
+epsilon = epsilon_transf_PLM_colon[index_PLM_colon]
+print("\t\tPLM\n")
+print("\t\t\tPLM epsilon scale:\t%0.3f"%(epsilon))
 # Allocate memory for a list
 R_PLM_trans = []
 t_PLM_trans = []
@@ -500,8 +504,12 @@ R_hat_PLM_inv_trans = np.array([fit_to_data.objective_PLM(inverse_parameters_PLM
 #------------------------------------------------------------------------------
 # STEP 1 OUT OF 4: TRANSFORM DATA
 #------------------------------------------------------------------------------
+# Find the index closest to the epsilon scale of the PLM
+index_IM_III_colon = np.where(epsilon_transf_IM_III_colon==epsilon_transf_IM_III_colon[np.abs(epsilon_transf_IM_III_colon-epsilon_scale_IM_III_colon).argmin()])[0][0]
 # Extract an epsilon value
-epsilon = epsilon_transf_IM_III_colon[len(epsilon_transf_IM_III_colon)-1]
+epsilon = epsilon_transf_IM_III_colon[index_IM_III_colon]
+print("\t\tIM-III\n")
+print("\t\t\tIM-III epsilon scale:\t%0.3f"%(epsilon))
 # Allocate memory for a list
 R_IM_III_trans = []
 t_IM_III_trans = []
